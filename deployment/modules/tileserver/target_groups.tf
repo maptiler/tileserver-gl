ed51a357539def279c1b2aac05981f8df4c5ceaa
Sokratis Siozos-Drosos <ssd@monosolutions.com>
2019-04-24 17:41:50 +0200

    (MODE-11365) Added slow_start and deregistration_delay to target_group.

--- deployment/modules/tileserver/target_groups.tf
+++ deployment/modules/tileserver/target_groups.tf
@@ -3,6 +3,8 @@ resource "aws_alb_target_group" "target_group_this" {
   port = "80"
   protocol = "HTTP"
   vpc_id = "${data.terraform_remote_state.mono_vpc.vpc_id[0]}"
+  slow_start = 120
+  deregistration_delay = 150
   stickiness {
     type            = "lb_cookie"
     enabled         = true
