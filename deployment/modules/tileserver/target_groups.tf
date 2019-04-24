653a090c3c42a09f90891838cbe0b5fc619c5411
Sokratis Siozos-Drosos <ssd@monosolutions.com>
2019-04-24 22:38:25 +0200

    (MODE-11365) Renamed target group.

--- deployment/modules/tileserver/target_groups.tf
+++ deployment/modules/tileserver/target_groups.tf
@@ -1,5 +1,5 @@
 resource "aws_alb_target_group" "target_group_this" {
-  name = "maps-test"
+  name = "maps"
   port = "80"
   protocol = "HTTP"
   vpc_id = "${data.terraform_remote_state.mono_vpc.vpc_id[0]}"
