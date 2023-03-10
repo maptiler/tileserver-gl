205e0af730c40f23b5b7ce2e2611c459906bc94c
a14stoner <59861665+a14stoner@users.noreply.github.com>
2023-03-10 10:50:58 +0100

    Update docker-entrypoint.sh
    fix error:
    
    what(): Failed to open X display
    
    by deleting the .X99-lock file at the start of the containe

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 if ! which -- "${1}"; then
   # first arg is not an executable
+  rm /tmp/.X99-lock -f > /dev/null || ``
   export DISPLAY=:99
   Xvfb "${DISPLAY}" -nolisten unix &
   exec node /usr/src/app/ "$@"
