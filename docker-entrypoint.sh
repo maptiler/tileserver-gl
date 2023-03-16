82c8e73a9082f30a552e7cd7b7766924f2a74c8c
zstadler <zeev.stadler@gmail.com>
2023-03-16 11:45:13 +0200

    Launch Xvfb only if needed
    Resolve #794

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,8 @@
 if ! which -- "${1}"; then
   # first arg is not an executable
   export DISPLAY=:99
-  Xvfb "${DISPLAY}" -nolisten unix &
+  # Launch Xvfb if needed
+  xdpyinfo --display "${DISPLAY}" >/dev/null 2>&1 || Xvfb "${DISPLAY}" -nolisten unix &
   exec node /usr/src/app/ "$@"
 fi
 
