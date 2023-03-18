b76d2b7c0ffe12a659f7a2f823c38ac7591c46d8
a14stoner <59861665+a14stoner@users.noreply.github.com>
2023-03-18 19:15:40 +0100

    Update docker-entrypoint.sh
    add check - if the file .X99-lock exists it will be removed. If not nothing is done.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 if ! which -- "${1}"; then
   # first arg is not an executable
-  rm /tmp/.X99-lock -f > /dev/null || ``
+  if [ -e /tmp/.X99-lock ]
+  then
+    rm /tmp/.X99-lock -f
+  fi
   export DISPLAY=:99
   Xvfb "${DISPLAY}" -nolisten unix &
   exec node /usr/src/app/ "$@"
