31f0c5632e679de8c10a665cee482160b69c0e8d
acalcutt <acalcutt@techidiots.net>
2023-04-02 22:46:12 -0400

    fix: change to const
    Signed-off-by: acalcutt <acalcutt@techidiots.net>

--- src/serve_rendered.js
+++ src/serve_rendered.js
@@ -19,7 +19,7 @@ import request from 'request';
 import { getFontsPbf, getTileUrls, fixTileJSONCenter } from './utils.js';
 
 import Os from 'os'
-var ostype = (Os.platform() == 'win32') ? "windows":"unix";
+const ostype = (Os.platform() == 'win32') ? "windows":"unix";
 const { sharp, createCanvas, Image } = await import(`./render_import_${ostype}.js`);
 
 const FLOAT_PATTERN = '[+-]?(?:\\d+|\\d+.?\\d+)';
