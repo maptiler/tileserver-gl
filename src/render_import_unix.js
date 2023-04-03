e1c46d4ef6fdc9fb4ce5f5863a36d0f03805129f
acalcutt <acalcutt@techidiots.net>
2023-04-02 23:11:14 -0400

    chore: comment update - required to import
    Signed-off-by: acalcutt <acalcutt@techidiots.net>

--- src/render_import_unix.js
+++ src/render_import_unix.js
@@ -1,6 +1,6 @@
 'use strict';
 
-// sharp has to be required before node-canvas on linux. see https://github.com/lovell/sharp/issues/371
+// sharp has to be imported before node-canvas on linux. see https://github.com/lovell/sharp/issues/371
 import sharp from 'sharp';
 import { createCanvas, Image } from 'canvas';
 
--- src/render_import_windows.js
+++ src/render_import_windows.js
@@ -1,6 +1,6 @@
 'use strict';
 
-// sharp has to be required after node-canvas on windows. see https://github.com/Automattic/node-canvas/issues/2155#issuecomment-1487190125
+// sharp has to be imported after node-canvas on windows. see https://github.com/Automattic/node-canvas/issues/2155#issuecomment-1487190125
 import { createCanvas, Image } from 'canvas';
 import sharp from 'sharp';
 
