c2adf642cfa4f8d0377935e2a7aae85a75abcb3a
acalcutt <acalcutt@techidiots.net>
2023-04-05 07:48:15 -0400

    fix: make os tag on the same line
    Signed-off-by: acalcutt <acalcutt@techidiots.net>

--- src/main.js
+++ src/main.js
@@ -54,8 +54,7 @@ program
 program.parse(process.argv);
 const opts = program.opts();
 
-console.log(`Starting ${packageJson.name} v${packageJson.version}`);
-console.log(`${os.version()} ${os.arch}`);
+console.log(`Starting ${packageJson.name} v${packageJson.version} - ${os.version()} ${os.arch}`);
 
 const startServer = (configPath, config) => {
   let publicUrl = opts.public_url;
