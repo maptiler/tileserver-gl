72427f9f886e8cfdab6da771e3dfa61865165a33
Tiago Costa <tdcosta100@gmail.com>
2023-06-30 23:39:34 -0300

    fix: lint errors
    Signed-off-by: Tiago Costa <tdcosta100@gmail.com>

--- install/canvas_sharp_compatibility.js
+++ install/canvas_sharp_compatibility.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 import * as process from 'process';
 import * as child_process from 'child_process';
@@ -7,10 +7,21 @@ import * as path from 'path';
 
 const originalPath = process.cwd();
 
-process.chdir(path.resolve(path.dirname(url.fileURLToPath(import.meta.url)), '..'))
+process.chdir(
+  path.resolve(path.dirname(url.fileURLToPath(import.meta.url)), '..'),
+);
 
-if (process.platform == 'win32' && (process.arch == 'ia32' || process.arch == 'x64')) {
-    child_process.spawn('powershell.exe', [ '-ExecutionPolicy', 'Bypass', '-File', 'install\\win32.ps1', process.arch ])
+if (
+  process.platform == 'win32' &&
+  (process.arch == 'ia32' || process.arch == 'x64')
+) {
+  child_process.spawn('powershell.exe', [
+    '-ExecutionPolicy',
+    'Bypass',
+    '-File',
+    'install\\win32.ps1',
+    process.arch,
+  ]);
 }
 
 process.chdir(originalPath);
