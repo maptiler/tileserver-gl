159c582d9ddc48a0428d22f1e666774a27b8c751
Phillip <phillipyates12@hotmail.com>
2026-07-27 15:23:35 -0500

    Update docker-entrypoint.sh shebang and cleanup
    Changed the shebang from bash to sh and removed unnecessary comments and commands.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,10 +1,7 @@
-#!/bin/bash
+#!/bin/sh
 if ! which -- "${1}"; then
-  # first arg is not an executable
-  if [ -e /tmp/.X99-lock ]; then rm /tmp/.X99-lock -f; fi
   export DISPLAY=:99
-  # Filter out harmless xkbcomp keysym warnings (high keycodes for Wayland not supported in X11)
-  Xvfb "${DISPLAY}" -nolisten unix 2> >(grep -vE "(Could not resolve keysym|XKEYBOARD keymap compiler|xkbcomp are not fatal)" >&2) &
+  Xvfb "${DISPLAY}" -nolisten unix &
   exec node /usr/src/app/ "$@"
 fi
 
